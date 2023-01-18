dd24420407fedfe77734fc333e9aafb78cc286f4
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-01-18 16:10:17 +0900

    config

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -11,35 +11,32 @@ export default defineConfig({
     },
     {
       title: 'Backers',
+      monthlyDollars: 10,
       preset: presets.small,
     },
     {
-      title: 'Sponsors',
-      monthlyDollars: 100,
-      preset: {
-        avatar: {
-          size: 42,
-        },
-        boxWidth: 52,
-        boxHeight: 52,
-        container: {
-          sidePadding: 30,
-        },
-      }
-    },
-    {
       title: 'Bronze Sponsors',
-      monthlyDollars: 500,
+      monthlyDollars: 100,
       preset: presets.medium,
     },
     {
       title: 'Silver Sponsors',
-      monthlyDollars: 1000,
+      monthlyDollars: 500,
       preset: presets.large,
     },
     {
       title: 'Gold Sponsors',
+      monthlyDollars: 1000,
+      preset: presets.large,
+    },
+    {
+      title: 'Platinum Sponsors',
       monthlyDollars: 2500,
+      preset: presets.large,
+    },
+    {
+      title: 'Diamond Sponsors',
+      monthlyDollars: 10000,
       preset: presets.large,
     },
   ]
